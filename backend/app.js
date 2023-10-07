b9702e4fff25472e74a549c5fb509f98f307430f
SsulsS <114453034+SsulsS@users.noreply.github.com>
2023-10-07 17:54:05 +0300

    beck

--- backend/app.js
+++ backend/app.js
@@ -17,7 +17,7 @@ const { PORT = 3000 } = process.env;
 
 const app = express();
 app.use(bodyParser.json());
-mongoose.connect('mongodb://localhost:27017/mestodb');
+mongoose.connect('mongodb://127.0.0.1:27017/mestodb');
 app.use(requestLogger);
 app.use(cors);
 app.get('/crash-test', () => {
